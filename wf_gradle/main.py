54da63e25173bd0defba83d66eafc8f35ed1f249
wancaibida <wancaibida@gmail.com>
2019-11-07 16:26:26 +0800

    add maven xml format dependency to gradle stype workflow.

--- wf_gradle/main.py
+++ wf_gradle/main.py
@@ -1,16 +1,22 @@
-import xml.etree.ElementTree as ET
-
+import json
+import sys
 import xmltodict
 
-data = '''
-<dependency>
-    <groupId>org.apache.commons</groupId>
-    <artifactId>commons-lang3</artifactId>
-    <version>3.9</version>
-</dependency>
 
-'''
+def main(xml_input):
+    data = xmltodict.parse(xml_input)
+    dependency = data['dependency']
+    return "{}:{}:{}".format(dependency['groupId'], dependency['artifactId'], dependency['version']).strip()
 
-data = xmltodict.parse(data)
-dependency = data['dependency']
-print("{}:{}:{}".format(dependency['groupId'], dependency['artifactId'], dependency['version']).strip())
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and len(sys.argv[1].strip()):
+        query = sys.argv[1]
+    else:
+        query = sys.stdin.read()
+
+    output = main(query)
+
+    result = {'items': [{'title': output, 'arg': output}]}
+    json.dump(result, sys.stdout)
+    sys.stdout.flush()
